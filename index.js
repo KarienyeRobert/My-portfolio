15bcdd2629e6b619f893caba48569d6aaba8bed6
KarienyeRobert <karienyerobert@gmail.com>
2023-11-16 10:38:24 +0300

    commit

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+//menu icon navbar
+let menuIcon= document.querySelector('#menu-icon')
+let navbar= document.querySelector('.navbar')
+
+menuIcon.onclick=()=>{
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
 //scroll sections active link
 let sections =document.querySelectorAll('section');
 let navLinks =document.querySelectorAll('header nav a');
-
-
-
-//sticky navbar
 
 window.onscroll=()=>{
   sections.forEach(sec=>{
@@ -21,9 +26,16 @@ window.onscroll=()=>{
     };
   });
 
+
+  //sticky navbar
 let header =document.querySelector('.header');
 
 header.classList.toggle('sticky',window.scrollY>100);
+
+//remove menu icon navbar 
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
 
 //popup tabs
@@ -76,3 +88,25 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+//dark light mode
+let darkModeIcon =document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick=()=>{
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode')
+}
+
+
+//scroll reveal
+ScrollReveal({
+  reset:true,
+  distance:'80px',
+  duration:2000,
+  delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading',{origin:'top'});
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box,.testimonial-wrapper, .contact form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img img',{origin:'left'});
+ScrollReveal().reveal('.home-content h3,.home-content p,.about-content ',{origin:'right'});
